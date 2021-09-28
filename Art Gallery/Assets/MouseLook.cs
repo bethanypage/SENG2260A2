f972d8ddc468cb23278ec4f24b75495d5b367b12
Bethany <bethany.p@live.com.au>
2021-09-28 21:39:14 +1000

    Initial project
    Base project commit

---
+++ Art Gallery/Assets/MouseLook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 1f;
+    public Transform personBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        personBody.Rotate(Vector3.up * mouseX);
+        
+
+
+
+    }
+}
