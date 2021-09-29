db8bcacaba1babaa160f0e752cac770263444e06
bethany <bethany.p@live.com.au>
2021-09-29 12:32:21 +1000

    Art Object UI pop up flow

--- Art Gallery/Assets/Scripts/ArtObject.cs
+++ Art Gallery/Assets/Scripts/ArtObject.cs
@@ -9,13 +9,38 @@ public class ArtObject : MonoBehaviour
     [SerializeField] Material normalBorderMat;
     [SerializeField] Material hightlightedBorderMat;
 
+    [Header("UI")]
+    [SerializeField] GameObject infoButton;
     [SerializeField] GameObject infoPanel;
-
+    bool showPanel;
 
     private void Awake()
     {
+        infoButton.SetActive(false);
         infoPanel.SetActive(false);
+        showPanel = infoPanel.activeSelf;
         artBorder.material = normalBorderMat;
+    }
+
+    #region InfoButton
+    void ShowInfoButton()
+    {
+        infoButton.SetActive(true);
+    }
+
+    void HideInfoButton()
+    {
+        infoButton.SetActive(false);
+    }
+    #endregion InfoButton
+
+    #region InfoPanel
+    public void ToggleInfoPanel()
+    {
+        showPanel = !showPanel;
+
+        if (showPanel) { ShowInfoPanel(); }
+        else { HideInfoPanel(); }
     }
 
     public void ShowInfoPanel()
@@ -27,28 +52,36 @@ public class ArtObject : MonoBehaviour
     {
         infoPanel.SetActive(false);
     }
+    #endregion InfoPanel
 
 
     #region Collider Trigger Events
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
+        {
             artBorder.material = hightlightedBorderMat;
-        //ShowInfoPanel();
+            ShowInfoButton();
+        }
     }
 
+    /*
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
             artBorder.material = hightlightedBorderMat;
         //ShowInfoPanel();
     }
+    */
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
+        {
             artBorder.material = normalBorderMat;
-        //HideInfoPanel();
+            HideInfoButton();
+            HideInfoPanel();
+        }
     }
     #endregion Collider Trigger Events
 }
